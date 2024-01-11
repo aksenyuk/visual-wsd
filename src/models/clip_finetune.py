503784054b0ae37e3699af4f858238afacecb938
Alessandro <oleksandr.yasinskyi@icloud.com>
2024-01-11 22:17:11 +0100

    fixed comment locations in CLIP_finetune class

--- src/models/clip_finetune.py
+++ src/models/clip_finetune.py
@@ -7,10 +7,10 @@ class CustomCLIPModel(BaseModel):
     def __init__(self, model_name: str):
         super(CustomCLIPModel, self).__init__()
         self.model = CLIPModel.from_pretrained(model_name)
-        # Cuda
         self.processor = CLIPProcessor.from_pretrained(model_name, do_rescale=False)
-        # Apple silicon
+        # CUDA
         # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # Apple silicon
         self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
         self.model.to(self.device)
 
